52dc276b35f1546c04f50e39606a452bd0a4d794
ChewZL <chewzl@yahoo.com.sg>
2016-09-06 22:26:31 +0800

    Using inheritance to achieve code reuse

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,31 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class Contact {
+	
+	private String value;
+    private boolean isPrivate;
+	
+	protected Contact(){
+	}
+	public String toString(){
+		return value;
+	}
+	
+	@Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact)other).value)); // state check
+    }
+	
+	@Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
